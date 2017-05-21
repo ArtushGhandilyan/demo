774b7c0fc114d132947a3bf5ddbf7182725dbf75
Artush Ghandilyan <artush.ghandilyan@gmail.com>
2017-05-21 18:29:52 +0400

    TEST-3

--- src/main/java/com/eyesoft/DemoApplication.java
+++ src/main/java/com/eyesoft/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("testing 3");
+		System.out.println("testing 4");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
